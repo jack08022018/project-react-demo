0458b198fa36556f3a2aea1efeae4a4c2ebedddd
jack08022018 <dinhtienhoang2009@gmail.com>
2023-10-30 15:54:25 +0700

    env

--- src/pages/history/Slice.js
+++ src/pages/history/Slice.js
@@ -13,6 +13,8 @@ export const Slice = createSlice({
     incrementCount: (state, action) => {
       console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
       console.log(`REACT_APP_ENVIRONMENT: ${process.env.REACT_APP_ENVIRONMENT}`);
+      console.log(`REACT_APP_FOO: ${process.env.REACT_APP_FOO}`);
+      console.log(`REACT_APP_BAR: ${process.env.REACT_APP_BAR}`);
       state.count = state.count + 1;
     },
   },
